ec7d099f977b717c6606f0e29c12e985c4e77b81
arsanchez5 <138083238+arsanchez5@users.noreply.github.com>
2024-06-10 18:10:04 -0500

    AccountTransactionService.java

--- src/main/java/com/banquito/core/account/service/AccountTransactionService.java
+++ src/main/java/com/banquito/core/account/service/AccountTransactionService.java
@@ -1,57 +1,27 @@
 package com.banquito.core.account.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.banquito.core.account.model.AccountTransaction;
-import com.banquito.core.account.repository.AccountTransactionRepository;
+import com.banquito.core.account.model.Account;
+import com.banquito.core.account.repository.AccountRepository;
 
 @Service
-public class AccountTransactionService {
+public class AccountService {
 
-    private final AccountTransactionRepository accountTransactionRepository;
+    private AccountRepository accountRepository;
 
-    public AccountTransactionService(AccountTransactionRepository accountTransactionRepository) {
-        this.accountTransactionRepository = accountTransactionRepository;
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
     }
 
-    public List<AccountTransaction> obtainAllAccountTransactions() {
+    public List<Account> obtainAllAccounts() {
         try {
-            return this.accountTransactionRepository.findAll();
+            return this.accountRepository.findAll();
         } catch (Exception e) {
-            throw new RuntimeException("Error al obtener todas las transacciones", e);
+            throw new RuntimeException("Error al obtener todas las cuentas", e);
         }
     }
 
-    public Optional<AccountTransaction> getAccountTransactionById(Integer id) {
-        try {
-            return accountTransactionRepository.findById(id);
-        } catch (Exception e) {
-            throw new RuntimeException("Error al obtener la transacción con ID: " + id, e);
-        }
-    }
-
-    public AccountTransaction obtainTransactionType(String transactionType) {
-        try {
-            List<AccountTransaction> transactionTypeList = this.accountTransactionRepository
-                    .findByTransactionType(transactionType);
-            if (!transactionTypeList.isEmpty()) {
-                return transactionTypeList.get(0); // Assuming you only need the first one
-            } else {
-                throw new RuntimeException("No existe una transacción con tipo: " + transactionType);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Error al buscar transacciones por tipo", e);
-        }
-    }
-
-    public List<AccountTransaction> findByState(String state) {
-        try {
-            return accountTransactionRepository.findByState(state);
-        } catch (Exception e) {
-            throw new RuntimeException("Error al buscar transacciones por estado", e);
-        }
-    }
 }
